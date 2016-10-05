82063a379a0f073f594fdf5f4e78a45861594c43
1lmar <shakirov_ilmar@mail.ru>
2016-10-05 21:35:42 +0300

    new

--- js/common.js
+++ js/common.js
@@ -1,32 +1,15 @@
 $(function() {
 
-	$(".owl-carousel").owlCarousel({
-		stagePadding: 10,
-		loop: true,
-		margin: 10,
-		responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
-     }
-	});
-	$("body").on('click', '[href*="#"]', function(e){
-		var fixed_offset = 0;
-		$('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
-		e.preventDefault();
-	});
-	
-	// toggle-mnu
 	$(".toggle-mnu").click(function() {
-	  $(this).toggleClass("on");
-	  $(".top-mnu").slideToggle();
-	  return false;
+	 $( ".menu-wrap" ).addClass( "active-menu" );
+	});
+	$(".close-button").click(function() {
+	 $( ".menu-wrap" ).removeClass( "active-menu" );
 	});
 
+	$(".mouse-icon").click(function(){
+		$("html, body").animate({
+			scrollTop: $("section").offset().top
+		}, 800);
+	});
 });
