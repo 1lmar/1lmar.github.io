d09229045e8f41ec2338f6b37593f7470b9bce10
1lmar <shakirov_ilmar@mail.ru>
2016-10-07 22:20:56 +0300

    portfolio

--- js/common.js
+++ js/common.js
@@ -1,15 +1,32 @@
 $(function() {
 
 	$(".toggle-mnu").click(function() {
-	 $( ".menu-wrap" ).addClass( "active-menu" );
+		$(this).toggleClass("on");
+
+		$(".menu li a").click(function(){
+			$(".menu").fadeOut(600);
+			$(".toggle-mnu").removeClass("on")
+		});
+
+		if($(".menu").is(":visible")){
+			$(".menu").fadeOut(600);
+			$(".menu li a").removeClass("fadeInUp animated");
+		}else{
+			$(".menu").fadeIn(600);
+			$(".menu li a").addClass("fadeInUp animated");
+		}
+		return false;
 	});
-	$(".close-button").click(function() {
-	 $( ".menu-wrap" ).removeClass( "active-menu" );
-	});
+
+	$(".menu ul a").mPageScroll2id();
+
 
 	$(".mouse-icon").click(function(){
 		$("html, body").animate({
 			scrollTop: $("section").offset().top
 		}, 800);
 	});
+
+	$(".portfolio-wrap").mixItUp();
+
 });
