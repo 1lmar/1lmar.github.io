61c19a6991bce2f9432ce25ae4f339b858b38d7e
1lmar <shakirov_ilmar@mail.ru>
2016-11-02 22:36:44 +0300

    mobile

--- js/common.js
+++ js/common.js
@@ -1,12 +1,17 @@
 $(function() {
 
-	$(".slider-row").owlCarousel({
-			items : 2,
-			margin: 10,
-			loop: true,
-			autoplay:true,
-			autoplayTimeout:2000,
-			autoplayHoverPause:true
-	});
+    $("body").addClass("js");
+  
+    $(".link-mnu").click(
+      function(){
+        $("body").toggleClass("show-menu");
+      }
+    );
+    $(".button").click(
+      function(){
+        $("body").toggleClass("show-menu");
+      }
+    );
+
 
 });
