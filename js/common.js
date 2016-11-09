f590a358f663464ebc063e9a0bbf8c3a5ff6ef0d
1lmar <shakirov_ilmar@mail.ru>
2016-11-10 00:12:51 +0300

    newwwwww

--- js/common.js
+++ js/common.js
@@ -2,22 +2,22 @@ $(function() {
 
     $("body").addClass("mobile-mnu");
 
-    $('.tabs-nav li').click(function(e) {
-      var a = $(this),
-          parent = a.parents('.tabs'),
-          nav = parent.children('.tabs-nav').children('li'),
-          box = parent.children('.tabs-box').children('div');
+    // $(".tabs-nav .owl-item").click(function(e) {
+    //   var a = $(this),
+    //       parent = a.parents('.tabs'),
+    //       nav = parent.children('.tabs-nav').children('.owl-item'),
+    //       box = parent.children('.tabs-box').children('div');
      
-      if (!a.hasClass('active')) {
-        a.addClass('active')
-          .siblings().removeClass('active');
+    //   if (!a.hasClass('active')) {
+    //     a.addClass('active')
+    //       .siblings().removeClass('active');
      
-        box.eq(a.index()).addClass('active')
-          .siblings().removeClass('active');
-      }
+    //     box.eq(a.index()).addClass('active')
+    //       .siblings().removeClass('active');
+    //   }
      
-      e.preventDefault();
-    });
+    //   e.preventDefault();
+    // });
 
     $(".log-item").magnificPopup({
       type:'inline',
