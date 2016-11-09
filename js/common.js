ef0b1ff400e5102d0a962e353854a9e6bddec024
1lmar <shakirov_ilmar@mail.ru>
2016-11-09 23:02:46 +0300

    tubbsss

--- js/common.js
+++ js/common.js
@@ -2,6 +2,23 @@ $(function() {
 
     $("body").addClass("mobile-mnu");
 
+    $('.tabs-nav li').click(function(e) {
+      var a = $(this),
+          parent = a.parents('.tabs'),
+          nav = parent.children('.tabs-nav').children('li'),
+          box = parent.children('.tabs-box').children('div');
+     
+      if (!a.hasClass('active')) {
+        a.addClass('active')
+          .siblings().removeClass('active');
+     
+        box.eq(a.index()).addClass('active')
+          .siblings().removeClass('active');
+      }
+     
+      e.preventDefault();
+    });
+
     $(".log-item").magnificPopup({
       type:'inline',
       midClick: true,
