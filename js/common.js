a82f6e8243b7299ca874d4dc49395de575bc4b1b
1lmar <shakirov_ilmar@mail.ru>
2016-11-21 23:35:19 +0300

    accordion

--- js/common.js
+++ js/common.js
@@ -34,21 +34,4 @@ $(function() {
 		autoplayHoverPause:true,
 	});
 
-	$('#accordion > li').hover(
-		function () {
-			var $this = $(this);
-			$this.stop().animate({'width':'480px'},500);
-			$('.heading',$this).stop(true,true).fadeOut();
-			$('.bgDescription',$this).stop(true,true).slideDown(500);
-			$('.description',$this).stop(true,true).fadeIn();
-		},
-		function () {
-			var $this = $(this);
-			$this.stop().animate({'width':'115px'},1000);
-			$('.heading',$this).stop(true,true).fadeIn();
-			$('.description',$this).stop(true,true).fadeOut(500);
-			$('.bgDescription',$this).stop(true,true).slideUp(700);
-		}
-		);
-
 });
