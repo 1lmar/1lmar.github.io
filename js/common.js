32924f6aeceaec1c131a723fd47cae70c17a37fd
1lmar <shakirov_ilmar@mail.ru>
2016-11-22 15:33:01 +0300

    scrolltop

--- js/common.js
+++ js/common.js
@@ -40,5 +40,16 @@ $(function() {
 		autoplayTimeout:4000,
 		autoplayHoverPause:true,
 	});
+	$("body").on("click", ".top", function(){
+		$("html, body").animate({scrollTop: 0}, "slow");
+	});
+	$("body").append('<div class="top"><i class="fa fa-angle-up">');
+	$(window).scroll(function(){
+		if($(this).scrollTop() > $(this).height()){
+			$(".top").addClass("active");
+		} else {
+			$(".top").removeClass("active");
+		}
+	});
 
 });
