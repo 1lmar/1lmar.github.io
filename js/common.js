08779f3ae526a97054da643d66e67b716e7c6d30
1lmar <shakirov_ilmar@mail.ru>
2016-11-23 17:50:30 +0300

    brands

--- js/common.js
+++ js/common.js
@@ -59,4 +59,36 @@ $(function() {
 		removalDelay: 300,
 	});
 
+	$('.brands-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-zoom-in',
+		gallery: {
+			enabled: true
+		},
+			removalDelay: 300,
+			callbacks: {
+				beforeChange: function() {
+					this.items[0].src = this.items[0].src + '?=' + Math.random(); 
+				},
+				open: function() {
+					$.magnificPopup.instance.next = function() {
+						var self = this;
+						self.wrap.removeClass('mfp-image-loaded');
+						setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+					}
+					$.magnificPopup.instance.prev = function() {
+						var self = this;
+						self.wrap.removeClass('mfp-image-loaded');
+						setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+					}
+				},
+				imageLoadComplete: function() { 
+					var self = this;
+					setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+				}
+			}
+	});
+
 });
