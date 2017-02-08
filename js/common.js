33aa29be4981b6813dd98929db18ed7fb1afbfc6
1lmar <shakirov_ilmar@mail.ru>
2017-02-08 23:27:35 +0300

    shirts

--- js/common.js
+++ js/common.js
@@ -1,17 +1,7 @@
 $(function() {
 
-	$(".main-slide").owlCarousel({
+	$('.head__shirt-slider').owlCarousel({
 		items: 1,
-		loop: true,
-		autoplay:true,
-		autoplayTimeout:5000,
-		autoplayHoverPause:true
 	});
-
-	$(".game-block").not(":first-child").hide();
-	$(".top-mnu .tab").click(function() {
-		$(".top-mnu .tab").removeClass("active").eq($(this).index()).addClass("active");
-		$(".game-block").hide().eq($(this).index()).fadeIn().addClass("active")
-	}).eq(0).addClass("active");
 
 });
