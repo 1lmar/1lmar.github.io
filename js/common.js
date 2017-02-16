f464fa59c075b9d98468092e060d461b9f3c64ac
1lmar <shakirov_ilmar@mail.ru>
2017-02-16 18:29:42 +0300

    descr

--- js/common.js
+++ js/common.js
@@ -3,5 +3,10 @@ $(function() {
 	$('.head__shirt-slider').owlCarousel({
 		items: 1,
 	});
+	$('.s__description-shirt--img').owlCarousel({
+		items: 1,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>']
+	});
 
 });
