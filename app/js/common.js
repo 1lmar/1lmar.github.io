84deeedd85596b9f19dea9445f15ab59c1eb017f
ilmar shakirov <shakirov_ilmar@mail.ru>
2017-11-01 15:21:49 +0300

    new

--- app/js/common.js
+++ app/js/common.js
@@ -7,4 +7,13 @@ $(function() {
     autoplayHoverPause:true
 	});
 
+	var o, n;
+  $(".title_block").on("click", function() {
+    o = $(this).parents(".accordion_item"), n = o.find(".info"),
+      o.hasClass("active_block") ? (o.removeClass("active_block"),
+        n.slideUp()) : (o.addClass("active_block"), n.stop(!0, !0).slideDown(),
+        o.siblings(".active_block").removeClass("active_block").children(
+          ".info").stop(!0, !0).slideUp())
+  });
+
 });
